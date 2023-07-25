e22e166186485ae35a7887063c665e6a4341527d
Phan Trọng Nhân <92132592+PTN407@users.noreply.github.com>
2023-07-26 00:37:41 +0700

    Update yolo.py

--- models/yolo.py
+++ models/yolo.py
@@ -133,9 +133,9 @@ class SimplePositionalEncoding(nn.Module):
     def forward(self, x1):
         res = []
         for x in x1:
-            xx = torch.stack([torch.unsqueeze((torch.arange(xi.shape[1])/xi.shape[1]).repeat(xi.shape[0],1),2) for xi in x])
-            xy = torch.stack([torch.unsqueeze((torch.arange(xi.shape[0])/xi.shape[0]).repeat(xi.shape[1],1).t(),2) for xi in x])
-            res.append(torch.cat((x,xx,xy),3))
+            xx = torch.stack([torch.unsqueeze((torch.arange(xi.shape[1])/xi.shape[1]).repeat(xi.shape[0],1),2) for xi in x]).to('cuda:0')
+            xy = torch.stack([torch.unsqueeze((torch.arange(xi.shape[0])/xi.shape[0]).repeat(xi.shape[1],1).t(),2) for xi in x]).to('cuda:0')
+            res.append(torch.cat((x,xx,xy),3).to('cuda:0'))
         return res
         
 class CLLABlock(nn.Module):
